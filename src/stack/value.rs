7c7069effda0cc7bf3773a04eb90af28c9a3f41e
mendess <pedro.mendes.26@gmail.com>
2021-05-08 00:02:53 +0100

    impl string concat

--- src/stack/value.rs
+++ src/stack/value.rs
@@ -264,7 +264,9 @@ macro_rules! impl_math {
 impl_math!(ops::Add, add {
     (Self::Char(c), Self::Integer(i)) => Self::Char((c as u8 + i as u8) as char),
     (Self::Str(s1), Self::Str(s2)) => Self::Str(s1 + &s2),
-    (Self::Str(mut s), Self::Char(c)) => Self::Str({ s.push(c); s }),
+    (Self::Str(s), any) => Self::Str(
+        s + &if let Value::Str(s) = any.to_str()? { s } else { unreachable!() }
+    ),
     (Self::Char(c), Self::Str(mut s)) => Self::Str({ s.push(c); s }),
     (Self::Array(mut a1), Self::Array(a2)) => {
         a1.extend(a2);
